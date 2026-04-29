0f0c252dbe581be18b3ded8210c2c15896b64190
Emmanuel Akinteye <140193411+Emmanuel4christ@users.noreply.github.com>
2026-04-29 11:13:32 -0700

    Add files via upload

---
+++ pages/home.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+$newnPageSlug = 'home';
+require __DIR__ . '/../lib/public-page.php';
+
---
+++ pages/pulse.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+$newnPageSlug = 'pulse';
+require __DIR__ . '/../lib/public-page.php';
+
